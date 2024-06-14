32698fe9e8839a0f867d8e80cd867447f7d490a6
Bhrigu verma <bhriguverma.bt23cse@pec.edu.in>
2024-06-14 14:14:26 +0530

    done animations pending now

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { FaArrowRight } from "react-icons/fa";
 function Card({width,start,para,hover}) {
   return (
-    <div className={`w-full bg-black ${hover} ${width}`}>
-    <div className={` max-w-screen-xl flex-col justify-between text-white rounded-xl p-5 flex min-h-[30rem] `}>
+    <div className={`rounded-2xl w-full bg-black ${hover} ${width}`}>
+    <div className={` max-w-screen-xl flex-col justify-between text-white  p-5 flex min-h-[30rem] `}>
    <div>
     <div className='text-black w-full flex'>
-    <h3 className='ml-5 pl-10'>Heading d</h3>
-    <FaArrowRight />
+    <h3 className='ml-5 pl-10'></h3>
+   
     </div>
     <h1 className='text-3xl font-medium mt-10'>Whatever heading</h1>
     </div>
@@ -16,7 +16,7 @@ function Card({width,start,para,hover}) {
     <div className='down w-full mt-60 '>
     {
     start===true && (   <> <h1 className='text-6xl font-semiblod leading-none '> Start a  project</h1>
-        <button className='flex  rounded-full mt-5 text-black bg-zinc-100 border-zinc-200 py-3 px-3 m-2'> contact us <div className="mx-2 my-1"><FaArrowRight /></div> </button> </>) 
+        <button className='flex  rounded-full mt-5 text-white border-4 border-sky-100 py-3 px-3 m-2'> contact us <div className="mx-2 my-1"><FaArrowRight /></div> </button> </>) 
     }
 { para===true && ( <p className='text-sm font-medium mt-3'> hello there how are you i am fine </p>)
     
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,8 +4,8 @@ function Cards() {
   return (
     <div className='bg-inherit w-full '>
     <div className='max-w-screen-xl mx-auto py-20  flex '>
-  <Card width="w-1/3" start={false} para={true} hover={"hover:bg-violet-600"}/>
-   <Card width={"w-2/3"} start={true} para={false} hover={"hover:bg-purple-600"}/>
+  <Card width={"w-2/3"} start={false} para={true} hover={"hover:bg-violet-700"}/>
+   <Card width={"w-1/3"} start={true} para={false} hover={"hover:bg-purple-600"}/>
    
     </div>
     </div>
